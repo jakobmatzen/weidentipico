0b7c157707d07b925f95ad83b1111609ecea522c
jakobmatzen <neinkobmatzen@gmail.com>
2025-07-14 20:41:13 +0200

    fix notification-store: added missing iterate-function

--- app.vue
+++ app.vue
@@ -1,5 +1,19 @@
 <script setup lang="ts">
 await usePrefetchStore().fetchData()
+
+const { notificationList } = storeToRefs(useNotificationStore())
+
+watch(notificationList.value, () => {
+  if (notificationList.value.length > 0) {
+    useToast().add({
+      title: notificationList.value[0].title,
+      description: notificationList.value[0].message,
+      color: notificationList.value[0].color,
+      duration: 5000,
+    })
+    notificationList.value.shift()
+  }
+})
 </script>
 
 <template>
