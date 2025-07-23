d0045f911477095f087933a5572b6b64d7068821
jakobmatzen <neinkobmatzen@gmail.com>
2025-07-23 20:34:00 +0200

    add date-picker

---
+++ components/shared/DatePicker.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { CalendarDate, DateFormatter, getLocalTimeZone, parseDate } from '@internationalized/date'
+
+const props = defineProps<{
+    placeholder?: string
+    minValue?: CalendarDate
+    maxValue?: CalendarDate
+}>()
+
+const emit = defineEmits(['select'])
+
+const date = defineModel<Date | null>()
+
+const df = new DateFormatter('de-DE', {
+    dateStyle: 'medium'
+})
+
+const modelValue = shallowRef(date.value
+    ? parseDate(new Date(date.value).toLocaleDateString('de-DE', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit'
+    }).split('.').reverse().join('-'))
+    : undefined)
+
+const placeholder = shallowRef(modelValue.value ? modelValue.value : new CalendarDate(new Date().getFullYear(), new Date().getMonth() + 1, new Date().getDate()))
+
+function update() {
+    if (modelValue.value) {
+        date.value = new Date(modelValue.value.year, modelValue.value.month - 1, modelValue.value.day)
+    }
+    else {
+        date.value = undefined
+    }
+}
+</script>
+
+<template>
+    <UPopover>
+        <UButton color="neutral" variant="outline" class="w-full"
+            :class="!modelValue && !props.placeholder ? 'text-dimmed' : ''">
+            {{ modelValue ? df.format(modelValue.toDate(getLocalTimeZone())) : props.placeholder ? props.placeholder :
+                'Datum...' }}
+        </UButton>
+        <template #content>
+            <UCalendar v-model="modelValue" v-model:placeholder="placeholder" class="p-2" :min-value="props.minValue"
+                :max-value="props.maxValue" @update:model-value="update(), emit('select')" />
+        </template>
+    </UPopover>
+</template>
